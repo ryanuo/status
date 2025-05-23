9df580d0ded4538bc66a349870f2d9ac800f4d86
ryanuo <1577791638@qq.com>
2025-05-23 14:42:27 +0800

    feat(DarkToggle): enhance theme toggle functionality
    - Rename toggleDark to toggleTheme for better clarity
    - Add support for cycling through light, dark, and system modes
    - Update UI to display

--- app/components/DarkToggle.vue
+++ app/components/DarkToggle.vue
@@ -9,13 +9,18 @@ useHead({
   }],
 })
 
-function toggleDark() {
-  color.preference = color.value === 'dark' ? 'light' : 'dark'
+function toggleTheme() {
+  const modes = ['light', 'dark', 'auto']
+  const currentIndex = modes.indexOf(color.preference)
+  const nextIndex = (currentIndex + 1) % modes.length
+  color.preference = modes[nextIndex] as string
 }
 </script>
 
 <template>
-  <button class="!outline-none cursor-pointer" @click="toggleDark">
-    <div class="i-carbon-sun dark:i-carbon-moon" />
+  <button class="!outline-none cursor-pointer" @click="toggleTheme">
+    <div v-if="color.preference === 'light'" class="i-carbon-sun" />
+    <div v-else-if="color.preference === 'dark'" class="i-carbon-moon" />
+    <div v-else class="i-carbon-ibm-cloud-sysdig-secure" /> <!-- system mode icon -->
   </button>
 </template>
